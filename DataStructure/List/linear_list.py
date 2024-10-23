73e2469cabc1a56727c1ed7c5f2605ec95b87a56
SeungHwa Lee <ox_____@naver.com>
2024-10-23 23:12:03 +0900

    241023_linear_list

--- DataStructure/List/linear_list.py
+++ DataStructure/List/linear_list.py
@@ -1,11 +1,37 @@
-# SIZE = 5
-# linear_list = [None for _ in range(SIZE)]
-li = []
+SIZE = 5
+linear_list = ["ON" for _ in range(SIZE)]
 
-def add_data(list, word):
-    global li
-    list[len(list)-1] = word
+# linear_list = []
+
+def add_data(word):
+    
+    linear_list.append(None)
+    list_len = len(linear_list)
+    linear_list[list_len-1] = word
+            
+
+def insert_data(word, position):
+
+    end = len(linear_list) - 1
+
+    if position >= end:
+        raise ValueError("It's impossible because the position exceeds the length of the list.")
+    
+    linear_list.append(None)
+    
+    for i in range(end,position,-1):
+        linear_list[i] = linear_list[i-1]
+        linear_list[i-1] = None
+
+    linear_list[position] = word
         
 
-def add_
 
+print(linear_list)
+
+insert_data("1번",1)
+insert_data("2번",2)
+
+print(linear_list)
+
+    
