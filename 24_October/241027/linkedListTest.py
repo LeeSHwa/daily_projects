3049d8988dec4d5df7eb3de7bf8a5e809c300c63
SeungHwa Lee <ox_____@naver.com>
2024-10-27 21:02:11 +0900

    linkList

--- 24_October/241027/linkedListTest.py
+++ 24_October/241027/linkedListTest.py
@@ -1,3 +1,8 @@
+'''
+['이름', 키] 의 형태인 2차원 리스트를 받아서
+키 순으로 오름차순 정렬 기능을 하는 Linked list를 만들 것임
+'''
+
 class Node():
     def __init__(self):
         self.data = None
@@ -14,28 +19,34 @@ def printNodes(start):
         print(current.data, end = ' ')
     print()
 
-def insertNode(findData, insertData):
-    global memory, head, current, pre
-
+def ascendingNumber(nameHeight):
+    global pre, current, head
+    printNodes(head)
     node = Node()
-    node.data = insertData
+    node.data = nameHeight
     
-    if head.data == findData:
+    if head is None: # 헤드가 없을 때
+        head = node
+        return
+    
+    if head.data[1] > nameHeight[1]:
         node.link = head
         head = node
-
         return
     
     current = head
     while current.link != None:
         pre = current
         current = current.link
-        if current.data == findData:
-            node.link = current
+
+        if current.data[1] > nameHeight[1]:
             pre.link = node
+            node.link = current
             return
-        
+    
     current.link = node
+
+
 
 def deleteNode(deleteData):
     global memory, head, current, pre
@@ -64,30 +75,16 @@ def deleteNode(deleteData):
 
 memory = []
 head = current = pre = None
-dataArray = ["다현", "정연", "쯔위", "사나", "지효"]
+dataArray = [ ["지민", 177] , ["정국",180] , ["뷔", 183] , ["슈가", 175] , ["진", 179] ]
 
 if __name__ == "__main__":
-
-    node = Node()               # 첫 번째 노드
-    node.data = dataArray[0]
-    head = node
-    memory.append(node)
-    
-    for data in dataArray[1:]: # 두 번째 이후 노드
-        pre = node
-        node = Node() # Node() 클래스의 데이터형을 불러와서 초기화
-        node.data = data 
-        pre.link = node
-        memory.append(node)
-
-insertNode("다현", "*해린*")
-insertNode("쯔위", "*혜인*")
-insertNode("지효", "*민지*")
+    cnt = 1
+    for data in dataArray :
+        
+        print(cnt, "-->", end = " ")
+        ascendingNumber(data)
+        cnt+=1
 
 printNodes(head)
 
-deleteNode("해린")
-printNodes(head)
 
-deleteNode("*해린*")
-printNodes(head)
