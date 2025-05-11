9a4d70d13ad2eeda628fada8a71cd4a4c2c6b04a
Lee SeungHwa <ox_____@naver.com>
2025-05-11 22:35:19 +0900

    java BOJ 11382 A+B+C, 입력조건 잘 보기

---
+++ baekjoon/BOJ(java)/Solved/BOJ11382.java
@@ -0,0 +1,15 @@
+package Solved;
+import java.util.Scanner;
+
+public class BOJ11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Long A = sc.nextLong();
+        Long B = sc.nextLong();
+        Long C = sc.nextLong();
+
+        System.out.println(A + B + C);
+    }
+    
+}
