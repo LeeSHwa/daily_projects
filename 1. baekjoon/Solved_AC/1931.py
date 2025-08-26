6346feee8ff54e001e83c47cbfb40a4a9e4a8efe
SeungHwa Lee <ox_____@naver.com>
2025-08-26 11:48:17 +0900

    solving...

--- 1. baekjoon/Solved_AC/1931.py
+++ 1. baekjoon/Solved_AC/1931.py
@@ -1,7 +1,9 @@
 # https://www.acmicpc.net/problem/1931
 
 # 문제 - 회의실 배정
+from collections import defaultdict
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
@@ -14,17 +16,15 @@ for _ in range(N):
     
 sorted_meeting = sorted(meeting)
 
-temp = {}
+temp = defaultdict(list)
 
 for i, j in sorted_meeting:
+    temp[i].append(j)
     
-    if temp[i]:
-        temp[i] = min(temp[i], j)
-        continue
-    
-    temp[i] = j
+    if len(temp[i]) > 1:
+        temp[i] = min(temp[i])
 
-print(temp)
+maximum_key = max(temp.keys())
 
 # 일단 새롭게 들어오는 meeting의 start가 기존에 있던 meeting의 end보다 크다면 추가?
 # cur_start > next_start and cur_end > next_end ??
@@ -32,3 +32,11 @@ print(temp)
 # 정렬을 어떻게 하지??
 # start 순으로 정렬을 해야하나?? 아니면 범위별로??
 
+N = list(len(temp))
+
+for start, end in temp:
+    
+
+    while 
+
+    
