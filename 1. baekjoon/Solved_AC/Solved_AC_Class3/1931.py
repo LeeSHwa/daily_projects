2dcf97ee4adc46aa45cfa6bcd3d65abca4155593
Lee SeungHwa <ox_____@naver.com>
2025-08-27 01:44:06 +0900

    1931_회의실 배정_SolvedAC3

--- 1. baekjoon/Solved_AC/Solved_AC_Class3/1931.py
+++ 1. baekjoon/Solved_AC/Solved_AC_Class3/1931.py
@@ -15,11 +15,11 @@ for _ in range(N):
     
 sorted_meeting = sorted(meeting, key=lambda x: (x[1], x[0]))
 
-pre_end = -1
+pre_end = 0
 count = 0
 
 for start, end in sorted_meeting:
-    if start > pre_end or (start == pre_end and start == end):
+    if start >= pre_end or (start == pre_end and start == end):
         pre_end = end
         count += 1
 
