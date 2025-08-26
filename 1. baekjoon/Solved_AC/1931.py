e245f425702e3f8cd42645f5a190e6d459420883
Lee SeungHwa <ox_____@naver.com>
2025-08-27 01:38:52 +0900

    1931_회의실 배정_SolvedAC3

--- 1. baekjoon/Solved_AC/1931.py
+++ 1. baekjoon/Solved_AC/1931.py
@@ -1,7 +1,6 @@
 # https://www.acmicpc.net/problem/1931
 
 # 문제 - 회의실 배정
-from collections import defaultdict
 import sys
 
 input = sys.stdin.readline
@@ -14,29 +13,14 @@ for _ in range(N):
     start, end = map(int, input().split())
     meeting.append([start, end])
     
-sorted_meeting = sorted(meeting)
+sorted_meeting = sorted(meeting, key=lambda x: (x[1], x[0]))
 
-temp = defaultdict(list)
+pre_end = -1
+count = 0
 
-for i, j in sorted_meeting:
-    temp[i].append(j)
-    
-    if len(temp[i]) > 1:
-        temp[i] = min(temp[i])
+for start, end in sorted_meeting:
+    if start > pre_end or (start == pre_end and start == end):
+        pre_end = end
+        count += 1
 
-maximum_key = max(temp.keys())
-
-# 일단 새롭게 들어오는 meeting의 start가 기존에 있던 meeting의 end보다 크다면 추가?
-# cur_start > next_start and cur_end > next_end ??
-
-# 정렬을 어떻게 하지??
-# start 순으로 정렬을 해야하나?? 아니면 범위별로??
-
-N = list(len(temp))
-
-for start, end in temp:
-    
-
-    while 
-
-    
+print(count)
