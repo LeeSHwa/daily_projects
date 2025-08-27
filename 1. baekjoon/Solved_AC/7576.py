e56404f4a408935b54b2df7364cac29de9b495c2
Lee SeungHwa <ox_____@naver.com>
2025-08-28 00:06:02 +0900

    solving...

---
+++ 1. baekjoon/Solved_AC/7576.py
@@ -0,0 +1,57 @@
+# https://www.acmicpc.net/problem/7576
+
+# 문제 - 토마토
+from collections import deque
+
+M, N = map(int, input().split()) # M : 가로, M : 세로
+
+tomato = []
+start_list = []
+
+for i in range(N):
+    line = list(map(int, input().split()))
+
+    for j in line:
+        if j == 1:
+            start_list.append((i, line.index(j)))
+    
+    tomato.append(line)
+
+
+def bfs(list):
+    queue = deque()
+    for start_row, start_column in list:
+        queue.append((start_row, start_column, 0))
+
+    direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    maximum_depth = -1 
+    visited = [[False] * M for _ in range(N)]
+
+    while queue:
+        row, col, depth = queue.popleft()
+        tomato[row][col] = min(depth + 1, tomato[row][col])
+        
+        for dr, dc in direction:
+            nr = row + dc
+            nc = col + dr 
+            
+            if 0 <= nr < N and 0 <= nc < M:
+                if not visited[nr][nc] and tomato[nr][nc] != -1:
+
+                    queue.append((nr, nc, depth + 1))
+                    visited[nr][nc] = True
+
+                maximum_depth = max(maximum_depth, tomato[nr][nc])
+    
+    return maximum_depth + 1
+
+answer = float('inf')
+answer = min(bfs(start_list), answer)
+
+for i in range(N):
+    for j in range(M):
+        if tomato[i][j] == 0:
+            print(-1)
+            exit()
+
+print(answer)
