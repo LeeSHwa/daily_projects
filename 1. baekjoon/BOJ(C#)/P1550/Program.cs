7874d179c39e5b7a09729fd1d3e452aa30bb4295
Lee SeungHwa <ox_____@naver.com>
2025-09-04 20:30:41 +0900

    Baekjoon: Solve P1550 in C#

---
+++ 1. baekjoon/BOJ(C#)/P1550/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        int x = Convert.ToInt32(input, 16);
+
+        Console.WriteLine(x);
+
+    }
+}
