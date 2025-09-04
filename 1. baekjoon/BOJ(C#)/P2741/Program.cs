db77aa744419a9b78445e728c96f5fe5fc462d53
Lee SeungHwa <ox_____@naver.com>
2025-09-04 21:32:31 +0900

    Baekjoon: Solve P2741 in C#

---
+++ 1. baekjoon/BOJ(C#)/P2741/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int N = int.Parse(Console.ReadLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++)
+        {
+            sb.AppendLine(i.ToString());
+        }
+        string answer = sb.ToString();
+
+        Console.WriteLine(answer);
+    }
+}
