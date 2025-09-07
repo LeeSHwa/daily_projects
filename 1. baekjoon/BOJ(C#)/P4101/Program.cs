281c818e226651e2f52397d753b826bca9cbfa9e
Lee SeungHwa <ox_____@naver.com>
2025-09-07 21:11:42 +0900

    Baekjoon: Solve P4101 in C#

---
+++ 1. baekjoon/BOJ(C#)/P4101/Program.cs
@@ -0,0 +1,36 @@
+﻿// https://www.acmicpc.net/problem/4101
+
+// 문제 - 크냐?
+
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool flag = false;
+
+        while (!flag)
+        {
+            string[] stringInputs = Console.ReadLine().Split();
+
+            int[] numbers = Array.ConvertAll(stringInputs, int.Parse);
+
+            if (numbers[0] == 0 && numbers[1] == 0)
+            {
+                flag = true;
+            }
+
+            else if (numbers[0] > numbers[1])
+            {
+                Console.WriteLine("Yes");
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
