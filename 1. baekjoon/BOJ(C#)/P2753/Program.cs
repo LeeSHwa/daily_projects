8c6eaffc23fea60f14b2499281be66d94b28f8d5
Lee SeungHwa <ox_____@naver.com>
2025-09-07 16:28:29 +0900

    Baekjoon: Solve P2753 in C#

---
+++ 1. baekjoon/BOJ(C#)/P2753/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://www.acmicpc.net/problem/2753
+
+// 문제 - 윤년
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+            Console.WriteLine(0);
+        }
+
+    }
+}
