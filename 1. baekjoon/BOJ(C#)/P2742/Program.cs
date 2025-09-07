27371f4419d05f8c63fca3832bd55ac841b5f8ee
Lee SeungHwa <ox_____@naver.com>
2025-09-07 15:52:59 +0900

    Baekjoon: Solve P2742 in C#

---
+++ 1. baekjoon/BOJ(C#)/P2742/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+class Program {
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = N; i > 0; i--)
+        {
+            sb.AppendLine(i.ToString());
+        }
+
+        string answer = sb.ToString();
+
+        Console.WriteLine(answer);
+    }
+}
