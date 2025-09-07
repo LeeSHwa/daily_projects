1f0a296983286d898b735d4416a7ba7c55af27a1
Lee SeungHwa <ox_____@naver.com>
2025-09-07 21:23:00 +0900

    Baekjoon: Solve P4562 in C#

---
+++ 1. baekjoon/BOJ(C#)/P4562/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://www.acmicpc.net/problem/4562
+
+// 문제 - No Brainer
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] str_inputs = Console.ReadLine().Split();
+            int[] numbers = Array.ConvertAll(str_inputs, int.Parse);
+
+            if (numbers[0] >= numbers[1])
+            {
+                Console.WriteLine("MMM BRAINS");
+            }
+
+            else
+            {
+                Console.WriteLine("NO BRAINS");
+            }
+        }
+    }
+}
