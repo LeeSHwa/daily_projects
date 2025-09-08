c842b806658d0b76adca559cc17e18101f29f980
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-09-08 08:10:10 +0000

    Baekjoon: Solve P5217 in C#

---
+++ 1. baekjoon/BOJ(C#)/P5217/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            bool is_odd = (N % 2 == 1) ? true : false;
+
+            if (is_odd)
+            {
+                int maximum = N / 2;
+                string[] pair = new string[maximum];
+
+                for (int j = 1; j <= maximum; j++)
+                {
+                    pair[j - 1] = j.ToString() + " " + (N - j).ToString();
+                }
+
+                Console.WriteLine("Pairs for " + N.ToString() + ": " + String.Join(", ", pair));
+            }
+
+            else
+            {
+                int maximum = N / 2;
+                string[] pair = new string[maximum - 1];
+                for (int j = 1; j < maximum; j++)
+                {
+                    pair[j - 1] = j.ToString() + " " + (N - j).ToString();
+                }
+
+                Console.WriteLine("Pairs for " + N.ToString() + ": " + String.Join(", ", pair));
+            }
+
+        }
+    }
+}
