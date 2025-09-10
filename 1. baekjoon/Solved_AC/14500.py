e1f7da27c81a37f6e862b4e754833e2132cf6dc2
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-09-10 02:57:13 +0000

    solving...

--- 1. baekjoon/Solved_AC/14500.py
+++ 1. baekjoon/Solved_AC/14500.py
@@ -3,6 +3,23 @@
 # 문제 - 테트로미노
 
 N, M = map(int, input().split())
+grid = []
 
 for _ in range(N):
     line = list(map(int, input().split()))
+    grid.append(line)
+
+stack = []
+
+visited = [[False] * M for _ in range(N)]
+
+def DFS(col, row, depth, current_sum):
+
+    direction = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    
+    
+    for dcol, drow in direction:
+        ncol = col + dcol
+        nrow = row + drow
+
+        
