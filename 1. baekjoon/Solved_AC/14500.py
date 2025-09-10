f179ac40d8a9740587b2b39d0659d5d223f8884b
Lee SeungHwa <ox_____@naver.com>
2025-09-10 17:29:24 +0900

    solving...

--- 1. baekjoon/Solved_AC/14500.py
+++ 1. baekjoon/Solved_AC/14500.py
@@ -9,17 +9,39 @@ for _ in range(N):
     line = list(map(int, input().split()))
     grid.append(line)
 
-stack = []
-
 visited = [[False] * M for _ in range(N)]
 
+max_sum = 0
+
 def DFS(col, row, depth, current_sum):
+    global max_sum
+
+    if depth == 4:
+        max_sum = max(max_sum, current_sum)
+        return # 백트래킹
 
     direction = [(0, 1), (1, 0), (-1, 0), (0, -1)]
     
+    for dc, dr in direction:
+        nc = col + dc
+        nr = row + dr
+
+        if not (0 <= nc < N and 0 <= nr < M) or visited[nc][nr]:
+            continue
     
-    for dcol, drow in direction:
-        ncol = col + dcol
-        nrow = row + drow
+        visited[nc][nr] = True
+        DFS(nc, nr, depth + 1, current_sum + grid[nc][nr])
+
+        visited[nc][nr] = False
+
+for i in range(N):
+    for j in range(M):
+        visited[i][j] = True
+        DFS(i, j, 1, grid[i][j])
+        visited[i][j] = False
+
+        dc = [(0, 1), (0, -1)]
+        dr = [(1, 0), (-1, 0)]
 
         
+
