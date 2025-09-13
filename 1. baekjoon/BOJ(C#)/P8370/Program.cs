729af47901a5e85f4820b84adfc556fc8f8b8cec
Lee SeungHwa <ox_____@naver.com>
2025-09-13 23:36:02 +0900

    Baekjoon: Solve P8370 in C#

---
+++ 1. baekjoon/BOJ(C#)/P8370/Program.cs
@@ -0,0 +1,16 @@
+﻿// https://www.acmicpc.net/problem/8370
+
+// Plane
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine(line[0] * line[1] + line[2] * line[3]);
+    }
+}
