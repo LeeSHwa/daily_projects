e62d00bffd7aaf637636d8eebf7944e6e53de2ff
Lee SeungHwa <ox_____@naver.com>
2025-09-13 23:30:36 +0900

    Baekjoon: Solve P7891 in C#

---
+++ 1. baekjoon/BOJ(C#)/P7891/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://www.acmicpc.net/problem/7891
+
+// 문제 - Can you add this?
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine(numbers[0] + numbers[1]);
+        }
+    }
+}
