965c55ffeaf6a79dfd775601a2b11f534d5227f5
Lee SeungHwa <ox_____@naver.com>
2025-09-13 23:14:25 +0900

    Baekjoon: Solve P6778 in C#

---
+++ 1. baekjoon/BOJ(C#)/P6778/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int antennas = int.Parse(Console.ReadLine());
+        int eyes = int.Parse(Console.ReadLine());
+
+        // TroyMartian
+        if (antennas >= 3 && eyes <= 4)
+        {
+            Console.WriteLine("TroyMartian");
+        }
+
+        // VladSaturnian
+        if (antennas <= 6 && eyes >= 2)
+        {
+            Console.WriteLine("VladSaturnian");
+        }
+
+        // GraemeMercurian
+        if (antennas <= 2 && eyes <= 3)
+        {
+            Console.WriteLine("GraemeMercurian");
+        }
+
+    }
+}
