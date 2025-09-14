91e73f2e71329ec8f75447a013e23a39a9750f99
Lee SeungHwa <ox_____@naver.com>
2025-09-14 16:13:26 +0900

    Baekjoon: Solve P10699 in C#

---
+++ 1. baekjoon/BOJ(C#)/P10699/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://www.acmicpc.net/problem/10699
+
+// 오늘 날짜
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime todayDate = DateTime.Today;
+
+        Console.WriteLine(todayDate.ToString("yyyy-MM-dd"));
+    }
+}
