2ec262ec248674b2c81951fb4d734d1fd1b941cb
Lee SeungHwa <ox_____@naver.com>
2025-09-16 23:45:16 +0900

    Baekjoon: Solve P9316 in C#

---
+++ 1. baekjoon/BOJ(C#)/P9316/Program.cs
@@ -0,0 +1,18 @@
+﻿// https://www.acmicpc.net/problem/9316
+
+// 문제 - Hello Judge
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine($"Hello World, Judge {i + 1}!");
+        }
+    }
+}
