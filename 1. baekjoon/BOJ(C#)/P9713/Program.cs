ab3ad9bb292e353d3c471562459142dcc41be2e6
LEE SEUNGHWA <ox_____@naver.com>
2025-09-25 23:01:55 +0900

    Baekjoon: Solve P9713 in C#

---
+++ 1. baekjoon/BOJ(C#)/P9713/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            int sum = 0;
+
+            int number = int.Parse(Console.ReadLine());
+            for (int j = 1; j <= number; j += 2)
+            {
+                sum += j;
+            }
+
+            Console.WriteLine(sum);
+            }
+        }
+    }
