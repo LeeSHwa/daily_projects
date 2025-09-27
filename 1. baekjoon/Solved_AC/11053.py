9fa888e261348e184cd63f649b7ff0761bd91253
LEE SEUNGHWA <ox_____@naver.com>
2025-09-27 23:55:32 +0900

    solving...

--- 1. baekjoon/Solved_AC/11053.py
+++ 1. baekjoon/Solved_AC/11053.py
@@ -1,6 +1,19 @@
 # https://www.acmicpc.net/problem/11053
 
 # 문제 - 가장 긴 증가하는 부분 수열
+from collections import defaultdict
 
 N = int(input())
-A = map(int, input().split())
+A = list(map(int, input().split()))
+
+num_list = defaultdict(int)
+max_num_list = -1
+
+for i in range(N):
+    if (N - i) < max_num_list:
+        pass
+
+    
+    
+    
+    
