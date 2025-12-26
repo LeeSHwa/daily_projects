3b9ed302533139b388c644fc9d347e5020f237f3
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-12-26 20:17:37 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251226/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
+++ 251226/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -1,6 +1,8 @@
 def print_ones(row, col):
-    line = "*" * row
-    for _ in range(col):
+    line = "1" * col
+    for _ in range(row):
         print(line)
 
 row, col = map(int, input().split())
+
+print_ones(row, col)
