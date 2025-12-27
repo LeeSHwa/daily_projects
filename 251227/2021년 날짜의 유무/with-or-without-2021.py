f8d3c86a493d53186534d24428ad1771e4179976
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-12-27 16:18:37 +0900

    Title: 2021년 날짜의 유무, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251227/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,22 @@
+def is_real(Month, Day):
+        
+    #                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+    num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    # day_of_the_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+    if 0 < Month < 13 and 0 < day < 32:
+        if day > num_of_days[Month]:
+            return False
+        return True
+    else:
+        return False
+    
+M, D = map(int, input().split())
+
+if is_real(M, D):
+    print("Yes")
+else:
+    print("No")
+
+
