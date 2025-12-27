680d49f748c8639a8259e63455009cf0de99f317
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-12-27 15:19:20 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251227/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -0,0 +1,35 @@
+def is_odd(N):
+    if N < 10:
+        if N % 2 == 0:
+            return 1
+        else:
+            return 0
+    
+    else:
+        temp = 0
+
+        while N > 0:
+            temp += N % 10
+            N //= 10
+
+        if temp % 2 == 0:
+            return 1
+        else:
+            return 0
+
+
+era = [False, False] + [True] * 99
+for i in range(2, 11):
+    if era[i]:
+        for j in range(i*i, 101, i):
+            era[j] = False
+        
+A, B = map(int, input().split())
+
+count = 0
+
+for num in range(A, B+1):
+    if era[num]:
+        count += is_odd(num)
+
+print(count)
