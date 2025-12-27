72057e5abfe17e93eb48762f3b5ce14dd6f7fc97
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-12-27 10:26:47 +0900

    Title: 함수를 이용한 소수 판별, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251227/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 251227/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -3,10 +3,17 @@ import math
 def is_prime(N):
     sqrt_N = math.sqrt(N)
 
-    for i in range(2, sqrt_N + 1):
+    for i in range(2, int(sqrt_N + 1)):
         if N % i == 0:
             return False
 
     return True
 
 A, B = map(int, input().split())
+prime_num_list = []
+
+for i in range(A, B + 1):
+    if is_prime(i):
+        prime_num_list.append(i)
+
+print(sum(prime_num_list))
