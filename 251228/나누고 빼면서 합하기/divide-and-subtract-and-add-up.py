74d9a27a93cee1de2f1d68da27dd2acaba2f0d19
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-12-28 16:51:50 +0900

    Title: 나누고 빼면서 합하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251228/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 251228/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -4,16 +4,16 @@ array = list(map(int, input().split()))
 def calculator(array, index):
     sum_value = 0
 
-    sum_value += array[index]
-
-    while index >= 1: # 4 -> 2 -> 1 
-            
+    sum_value += array[index - 1] # 4
+ 
+    while index > 1: # (4 -> 2), (2 -> 1)
+        
         if index % 2 == 0:
             index //= 2
         else:
             index -= 1
 
-        sum_value += array[index - 1]
+        sum_value += array[index - 1] # 2, 1
 
     return sum_value
 
