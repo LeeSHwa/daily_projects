90bd938b36167fecd6a078ece2da6e2eb05d0771
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-12-30 10:23:44 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251230/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 251230/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -2,15 +2,15 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 def cal(n):
-    if arr[n] > 2:
+    if 2 in arr and arr[n] > 2:
         if arr[n] % 2 == 0:
             arr[n] //= 2
 
-    if arr[n] > 3:
+    if 3 in arr and arr[n] > 3:
         if arr[n] % 3 == 0:
             arr[n] //= 3
 
-    if arr[n] > 5:            
+    if 5 in arr and arr[n] > 5:            
         if arr[n] % 5 == 0:
             arr[n] //= 5
 
