869ef0b1f59e50e684031125235f3a8a1a5366c7
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2025-12-30 11:03:43 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251230/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 251230/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,25 +1,20 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a,b)
+
 def cal(n):
-    if 2 in arr and arr[n] > 2:
-        if arr[n] % 2 == 0:
-            arr[n] //= 2
-
-    if 3 in arr and arr[n] > 3:
-        if arr[n] % 3 == 0:
-            arr[n] //= 3
-
-    if 5 in arr and arr[n] > 5:            
-        if arr[n] % 5 == 0:
-            arr[n] //= 5
-
     if n == 0:
         return arr[0]
-
-    return cal(n - 1) * arr[n]
-
+    
+    prev_lcm = cal(n - 1)
+    return lcm(prev_lcm, arr[n])
 
 
 print(cal(n-1))
-    
