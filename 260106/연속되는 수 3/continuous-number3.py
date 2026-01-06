2575f7036f807a90d11504300e8718ea0c7e11b7
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-06 13:41:33 +0900

    Title: 연속되는 수 3, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 260106/연속되는 수 3/continuous-number3.py
+++ 260106/연속되는 수 3/continuous-number3.py
@@ -1,21 +1,18 @@
 N = int(input())
 
 array = [int(input()) for _ in range(N)]
-count = 0
-max_count = -1
-
-is_positive = False
+count = 1
+max_count = 1
 
 for i in range(N):
     if i == 0:
-        count = 1
         continue
     
     if array[i] * array[i-1] >= 0:
         count += 1
-        max_count = max(max_count, count)
-    
     else:
         count = 1
         
+    max_count = max(max_count, count)
+    
 print(max_count)
