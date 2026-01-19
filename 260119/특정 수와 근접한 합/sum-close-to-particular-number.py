a0b95c29e62d41bed81c59ca40b709605233b091
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-19 23:53:34 +0900

    Title: 특정 수와 근접한 합, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 260119/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 260119/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -10,9 +10,9 @@ for i in range(N-1):
     for j in range(i+1, N):
         temp = all_sum
         
-        aaa = temp - nums[i] - nums[j]
+        aaa = abs(temp - nums[i] - nums[j]) # 전체 합에서 두 수를 뺌
 
-        mins = min(mins, aaa)
+        mins = min(mins, abs(S - aaa))           
 
-print(mins - S)
+print(mins)                        # mins와 S의 차이
             
