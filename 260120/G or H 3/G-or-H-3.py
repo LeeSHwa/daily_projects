1973ab7ff35f0d208d70b4d5d67e4d6d2b6854f8
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-20 15:45:30 +0900

    Title: G or H 3, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 260120/G or H 3/G-or-H-3.py
+++ 260120/G or H 3/G-or-H-3.py
@@ -5,18 +5,19 @@ max_pos = -1
 for _ in range(N):
     pos, alaph = input().split()
     pos = int(pos)
-    if alaph == 'G':
-        alaph = 1
-    else:
-        alaph = 2
+    alaph = 1 if alaph == 'G' else 2
 
     position[pos] = alaph
     max_pos = max(max_pos, pos)
 
-first_sum = sum(position[1 : K])
+current_sum = sum(position[1 : K + 2]) # 1부터 K개의 구간을 넘어야 해서, 총 K + 1개의 지점을 포함
+# print(current_sum)
 
-sum_of_nums = [first_sum]
-for i in range(1, max_pos - K + 1):
-    sum_of_nums.append(first_sum - position[i] + position[i + K])
+sum_of_nums = [current_sum]
+for i in range(1, max_pos - K):
+    next_sum = current_sum - position[i] + position[i + K + 1]
+    current_sum = next_sum
+    sum_of_nums.append(next_sum)
+    # print(f"i : {i}, sum : {next_sum}")
 
 print(max(sum_of_nums))
