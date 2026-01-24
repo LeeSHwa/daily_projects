f04bf567776f12bc834e94a505cc53911f5ea2bd
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-24 11:31:25 +0900

    Title: 개발팀의 능력, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260124/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,29 @@
+# 3 5 1 4 2
+# (3, 1), (4, 2), 5
+# 팀 점수가 같으면 안됨
+# 이번엔 완탐으로 접근
+
+devs = list(map(int, input().split()))
+
+devs.sort() # 일단 정렬을 해
+
+min_diff = 2001
+
+for i in range(4):
+    for j in range(i + 1, 5):
+        team1 = devs[i] + devs[j]
+
+        for k in range(4):
+            for l in range(k + 1, 5):
+                
+                if k != i and l != j and k != i and l != j:
+                    team2 = devs[k] + devs[l]
+
+                    team3 = sum(devs) - team1 - team2
+                
+                    if team3 != team2 and team2 != team1:
+                        diff = max(team1, team2, team3) - min(team1, team2, team3)
+
+                        min_diff = min(min_diff, diff)
+
+print(min_diff)##
