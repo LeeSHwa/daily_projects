db7d5338176bed7b90d080300fb11d2ccea4d5f1
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-26 15:01:57 +0900

    Title: 삼각형 만들기, Time: 103ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 260126/삼각형 만들기/create-triangle.py
+++ 260126/삼각형 만들기/create-triangle.py
@@ -1,11 +1,10 @@
 n = int(input())
-offset = 10001
 
 points = [tuple(map(int, input().split())) for _ in range(n)]
 points.sort(key = lambda x : (x[0], x[1]))
 
-x = [p[0] + offset for p in points]
-y = [p[1] + offset for p in points]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
 
 max_extent = 0
 
@@ -13,12 +12,22 @@ for i in range(n - 2):
     for j in range(i + 1, n - 1):
         for k in range(j + 1, n):
             if x[i] != x[j] and y[i] == y[j] and x[j] == x[k] and y[j] != y[k] and x[i] != x[k] and y[i] != y[k]:
-                extent = (x[j] - x[i]) * (y[k] - y[j])
+                extent = abs(x[k] - x[i]) * abs(y[k] - y[i])
                 max_extent = max(max_extent, extent)
             
-            elif [i] == x[j] and y[i] != y[j] and x[j] != x[k] and y[j] == y[k] and x[i] != x[k] and y[i] != y[k]:
-                extent = (x[j] - x[i]) * (y[k] - y[j])
+            elif x[i] == x[j] and y[i] != y[j] and x[j] != x[k] and y[j] == y[k] and x[i] != x[k] and y[i] != y[k]:
+                extent = abs(x[k] - x[i]) * abs(y[k] - y[i])
                 max_extent = max(max_extent, extent)
             
+            elif x[i] != x[k] and y[i] != y[k] and x[j] == x[k] and y[j] != y[k] and x[i] != x[j] and y[i] != y[j]:
+                extent = abs(x[j] - x[i]) * abs(y[j] - y[i])
+                max_extent = max(max_extent, extent)
+            
+            elif x[i] != x[j] and y[i] == y[j] and x[j] != x[k] and y[j] != y[k] and x[i] == x[k] and y[i] != y[k]:
+                extent = abs(x[j] - x[k]) * abs(y[j] - y[k])
+                max_extent = max(max_extent, extent)
+            
+
+
 print(max_extent)
             
