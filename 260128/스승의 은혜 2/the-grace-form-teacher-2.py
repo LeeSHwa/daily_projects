53fce5fa602716932123db3fb226a3db4ad00e28
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-28 23:57:34 +0900

    Title: 스승의 은혜 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260128/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,23 @@
+N, B = map(int, input().split())
+
+prices = [int(input()) for _ in range(N)]
+
+prices.sort()
+
+price = 0
+cnt = 0
+
+for i in range(N):
+    if price <= B:
+        price += prices[i]
+        cnt += 1
+        continue
+    
+    temp = price + prices[i] // 2
+
+    if temp <= B:
+        price = temp
+        cnt += 1
+        continue
+
+print(cnt)
