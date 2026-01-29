5c0d1079bcd643804ac91fba4b8a8664ef9b6779
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-29 15:50:38 +0900

    Title: 상해버린 치즈, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260129/상해버린 치즈/rotten-cheese.py
+++ 260129/상해버린 치즈/rotten-cheese.py
@@ -3,51 +3,44 @@ from collections import defaultdict
 N, M, D, S = map(int, input().split())
 
 timeline1 = defaultdict(list)
-
-timeline2 = [0] * 102
+timeline2 = defaultdict(list)
 
 eat_mushroom = defaultdict(set)
-who_eat = defaultdict(set)
 
 for _ in range(D):
     p, m, t = map(int, input().split())
     timeline1[t].append((p, m))
     eat_mushroom[m].add(p)
-    who_eat[p].add(m)
 
 for _ in range(S):
     p, t = map(int, input().split())
-    timeline2[t] = p
+    timeline2[t].append(p)
 
 candidates = set([i for i in range(1, M + 1)])
-# print(candidates)
 
 for time in range(1, 101):
-    if timeline2[time]: # 만약 아팠다면 그 전의 먹은 사람과 머쉬룸을 조사
-        person = timeline2[time] 
+    if timeline2[time]:                   # 만약 아팠다면 그 전의 먹은 사람과 버섯을 조사
+    
+        for person in timeline2:
+    
+            current_cand = set()
+            
+            for in_time in range(1, time): # 아프기 전에 그 사람이 먹은 버섯을 후보에 추가
 
-        current_cand = set()
-        
-        for in_time in range(1, time):
+                if timeline1[in_time]:     # 만약 해당 시간에 누군가 먹은 흔적이 있다면
 
-            if timeline1[in_time]:
+                    for p, m in timeline1[in_time]: # 먹은사람과 버섯을 순회하면서
 
-                for p, m in timeline1[in_time]:
+                        if p == person:             # 만약 먹은사람이 아픈사람과 일치한다면
+                            current_cand.add(m)     # 현재후보에 추가
 
-                    if p == person:
-                        current_cand.add(m)
+            if len(current_cand):                       # 만약 현재 후보가 빈 값이 아니라면
+                candidates = candidates & current_cand  # 기존 후보군들과 교집합 연산을 통해 용의자 버섯을 추림
 
-        if len(current_cand):
-            # print(current_cand)
-
-            candidates = candidates & current_cand
-
-# print(*candidates)
 max_med = 0
 
 for candidate in candidates:
     curr_med = len(eat_mushroom[candidate])
     max_med = max(max_med, curr_med)
-    # print(candidate, *eat_mushroom[candidate], " len: ", curr_med)
 
 print(max_med)
