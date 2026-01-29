ceb9346f96ed0e6b13a17b313d0a3db06ee63f4e
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-29 14:04:37 +0900

    Title: 상해버린 치즈, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260129/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+N, M, D, S = map(int, input().split())
+
+timeline1 = defaultdict(list)
+
+timeline2 = [0] * 101
+
+eat_mushroom = defaultdict(list)
+
+for _ in range(D):
+    p, m, t = map(int, input().split())
+    timeline1[t].append((p, m))
+    eat_mushroom[m].append(p)
+
+for _ in range(S):
+    p, t = map(int, input().split())
+    timeline2[t] = p
+
+candidates = set()
+
+for time in range(1, 101):
+    if timeline2[time]: # 만약 아팠다면 그 전의 먹은 사람과 머쉬룸을 조사
+        person = timeline2[time] 
+        
+        for in_time in range(1, time):
+            
+            if timeline1[in_time]:
+                for p, m in timeline1[in_time]:
+                    candidates.add(m)
+        
+        break
+
+max_med = 0
+for candidate in candidates:
+    max_med = max(max_med, len(eat_mushroom[candidate]))
+
+print(max_med)
