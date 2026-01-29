0484154aa084561192ee3ca639975c806310c479
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-01-29 17:25:27 +0900

    Title: 이상한 폭탄 2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/이상한 폭탄 2/strange-bomb-2.py
+++ 260129/이상한 폭탄 2/strange-bomb-2.py
@@ -1,15 +1,17 @@
 N, K = map(int, input().split())
 
 booms = [int(input()) for _ in range(N)]
-is_booms = [False] * N
+is_boom = [False] * (1001)
 
+idx = -1
 for i in range(N - 1):
-    if i + 1 + K < N:
-        for j in range(i + 1, i + 1 + K):
-            if booms[i] == booms[j]:
-                is_booms[j] = True
 
-for i in range(N-1, -1, -1):
-    if is_booms[i] == True:
-        print(i)
-        break
+    bound = i + K if i + K < N else N - 1
+
+    if bound < N:
+        for j in range(i + 1, bound + 1):
+            # print(booms[i], booms[j])
+            if booms[i] == booms[j]:
+                idx = max(idx, booms[j])
+
+print(idx)
