c957840b1a9d25e59440f46c01d07dd986b14bae
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-02 16:09:20 +0900

    Title: 흥미로운 숫자 2, Time: 213ms, Memory: 18MB, Status: Passed - Codetree

--- 260202/흥미로운 숫자 2/interesting-numbers-2.py
+++ 260202/흥미로운 숫자 2/interesting-numbers-2.py
@@ -4,7 +4,19 @@ X, Y = map(int, input().split())
 
 cnt = 0
 for num in range(X, Y + 1):
-    if len(set(list(map(int, str(num))))) == 2:
-        cnt += 1
+    array = list(map(int, str(num)))
+    set_array = set(array)
 
+    if len(set_array) == 2:
+        a, b = set_array
+        len_array = len(array)
+
+        a_count = array.count(a)
+        b_count = len(array) - a_count
+
+        if a_count > 1 and b_count > 1:
+            continue
+    
+        cnt += 1
+        
 print(cnt)
