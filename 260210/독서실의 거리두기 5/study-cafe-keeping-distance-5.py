bdeb9d4259d299de11db59cbb79342e5e5c1a827
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-10 17:29:02 +0900

    Title: 독서실의 거리두기 5, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260210/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 260210/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,5 +1,5 @@
 N = int(input())
-seats = input()
+seats = list(map(int, input()))
 
 MAX_DISTANCE = 20
 
@@ -9,7 +9,7 @@ cnt = MAX_DISTANCE
 # LEFT
 for i in range(N):
 
-    if seats[i] == '1':
+    if seats[i] == 1:
         cnt = 0
     else:
         cnt += 1
@@ -20,11 +20,35 @@ cnt = MAX_DISTANCE
 # RIGHT
 for i in range(N - 1, -1, -1):
 
-    if seats[i] == '1':
+    if seats[i] == 1:
         cnt = 0
     else:
         cnt += 1
 
     distance[i] = min(distance[i], cnt)
 
-print(max(distance))
+max_distance = max(distance)
+
+candidates = [idx for idx, d in enumerate(distance) if d == max_distance]
+
+ans = float('inf')
+
+for idx in candidates:
+    seats[idx] = 1
+    
+    curr_distance = float('inf')
+    last_person = -1
+
+    for i in range(N):
+        if seats[i] == 1:
+            if last_person != -1:
+                curr_distance = min(curr_distance, i - last_person)
+            last_person = i
+
+
+    ans = min(ans, curr_distance)
+
+    seats[idx] = 0
+
+print(ans)
+    
