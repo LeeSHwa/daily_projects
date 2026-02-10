370019b1f60c68c373e5b909a0642f2c031d75d5
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-10 10:46:14 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260210/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 260210/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -5,12 +5,15 @@ min_length = float('inf')
 
 for length in range(1, N + 1):
     
+    flag = True
     for idx in range(N - length + 1):
         temp = string[idx : idx + length]
         
         if string.count(temp) > 1:
+            flag = False
             break
-        else:
-            min_length = min(min_length, len(temp))            
+    
+    if flag:
+        min_length = min(min_length, len(temp))            
 
 print(min_length)
