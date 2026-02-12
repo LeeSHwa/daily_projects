f440659575b93adf81ffdce476ee9d44a273a3d0
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-12 15:45:44 +0900

    Title: 수들의 최대 차, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260212/수들의 최대 차/maximum-difference-in-numbers.py
+++ 260212/수들의 최대 차/maximum-difference-in-numbers.py
@@ -1,5 +1,6 @@
 N, K = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
+arr.sort()
 
 # Please write your code here.
 
@@ -10,7 +11,7 @@ for i in range(N):
     curr_cnt = 0
     curr_min = float('inf')
     curr_max = 0
-    temp = []
+    temp = [arr[i]]
 
     for j in range(N):
         if j == i:
@@ -22,7 +23,8 @@ for i in range(N):
         curr_max = max(curr_max, max(temp))
         
         if curr_max - curr_min > K:
-            temp.remove(arr[j])
+            temp.pop()
+            break
     
     max_cnt = max(max_cnt, len(temp))
 
