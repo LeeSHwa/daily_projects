0b84c4a72e2348e68f17cb8dcc6c07b425ecba8a
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-24 23:51:04 +0900

    Title: 최대 H 점수 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260224/최대 H 점수 2/maximum-h-score-2.py
@@ -0,0 +1,28 @@
+from itertools import zip_longest
+
+N, L = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.sort()
+
+L_list = [1] * L
+
+def is_possible(H, arr):
+    cnt = 0
+    for num in arr:
+        if num >= H:
+            cnt += 1
+    
+    return cnt >= H
+
+ans = 0
+for h in range(101):
+    
+    temp = [x for x in nums if x >= h]
+
+    temp2 = [x + y for x, y in zip_longest(temp, L_list, fillvalue = 0)]
+
+    if is_possible(h, temp2):
+        ans = min(temp2)
+
+print(ans)
