47351015a197afc5122dac996df6e51d24fbc48b
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-26 16:13:52 +0900

    Title: 코딩톡, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260226/코딩톡/coding-talk.py
@@ -0,0 +1,14 @@
+N, M, p = map(int, input().split())
+timeline = {}
+
+programmers = [chr(x) for x in range(65, 65 + N)]
+
+for time in range(M):
+    timeline[time] = input().split()
+
+    if time >= (p - 1) and timeline[time][0] in programmers:
+        programmers.remove(timeline[time][0])
+
+print(*programmers)
+
+
