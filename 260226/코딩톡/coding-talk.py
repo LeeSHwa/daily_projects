fa0d9cb83cc812cae4b9fe70df44081fa315da44
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-26 16:35:46 +0900

    Title: 코딩톡, Time: 75ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260226/코딩톡/coding-talk.py
+++ 260226/코딩톡/coding-talk.py
@@ -2,6 +2,7 @@ N, M, p = map(int, input().split())
 timeline = {}
 
 programmers = [chr(x) for x in range(65, 65 + N)]
+last = []
 
 for time in range(M):
     timeline[time] = input().split()
@@ -9,6 +10,11 @@ for time in range(M):
     if time >= (p - 1) and timeline[time][0] in programmers:
         programmers.remove(timeline[time][0])
         
+        if last[0] in programmers and last[1] == timeline[p-1][1]:
+            programmers.remove(last[0])
+
+    last = timeline[time]
+
 if timeline[p - 1][1] == '0':
     exit()
 
