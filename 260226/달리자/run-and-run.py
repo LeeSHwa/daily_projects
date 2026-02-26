2c3c9cf61dbae6930aa40f8a7c6a636fa61f192f
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-26 15:25:33 +0900

    Title: 달리자, Time: 47ms, Memory: 17MB, Status: Passed - Codetree

--- 260226/달리자/run-and-run.py
+++ 260226/달리자/run-and-run.py
@@ -5,13 +5,10 @@ B = list(map(int, input().split()))
 
 diff = [a - b for a, b in zip(A, B)]
 
-length = 0
-for i in range(n - 1):
-    if diff[i] > 0:
-        length += diff[i]
-        diff[i+1] += diff[i]
-    else:
-        diff[i] += diff[i-1]
-        length += diff[i]
+ans = 0
+curr_diff = 0
+for i in range(n):
+    curr_diff += diff[i]
+    ans += curr_diff
 
-print(length)
+print(ans)
