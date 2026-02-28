2586b7e75c941cff061d46d75dbc68c853d5fe78
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-02-28 11:26:01 +0900

    Title: 독서실의 거리두기 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260228/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,41 @@
+n = int(input())
+
+seats = list(input())
+
+distance = [float('inf')] * n
+
+# left
+curr = float('inf')
+for i in range(n):
+    if seats[i] == '1':
+        curr = 0
+    else:
+        curr += 1
+    
+    distance[i] = min(distance[i], curr)
+
+# right
+curr = float('inf')
+for i in range(n-1, -1, -1):
+    if seats[i] == '1':
+        curr = 0
+    else:
+        curr += 1
+    
+    distance[i] = min(distance[i], curr)
+
+candidate = distance.index(max(distance))
+
+seats[candidate] = '1'
+
+last = 0
+min_distance = float('inf')
+for i in range(1, n):
+    curr_distance = 0
+    if seats[i] == '1':
+        curr_distance = i - last
+        last = i
+    
+        min_distance = min(min_distance, curr_distance)
+
+print(min_distance)
