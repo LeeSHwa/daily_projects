bbc37d4d5e2628e75c38db0ba11eef1266f02e12
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-03-01 17:52:54 +0900

    Title: L, R 그리고 B, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260301/L, R 그리고 B/l-r-and-b.py
@@ -0,0 +1,26 @@
+SIZE = 10
+
+grid = []
+L = ()
+R = ()
+B = ()
+
+for row in range(SIZE):
+    line = list(input())
+    grid.append(line)
+
+    for idx in range(SIZE):
+        if line[idx] == 'L':
+            L = (row, idx)
+        elif line[idx] == 'R':
+            R = (row, idx)
+        elif line[idx] == 'B':
+            B = (row, idx)
+
+distance = 0
+if (L[0] == R[0] and R[0] == B[0]) or (L[1] == R[1] and R[1] == B[1]):
+    distance = max(abs(B[0] - L[0]), abs(B[1] - L[1])) + 1
+else:
+    distance = abs(B[0] - L[0]) +  abs(B[1] - L[1]) - 1
+
+print(distance)
