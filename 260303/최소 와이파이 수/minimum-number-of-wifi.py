f91ce863de45565719da88557d10658c8adc1f01
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-03-03 17:45:22 +0900

    Title: 최소 와이파이 수, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260303/최소 와이파이 수/minimum-number-of-wifi.py
+++ 260303/최소 와이파이 수/minimum-number-of-wifi.py
@@ -3,9 +3,12 @@ arr = list(map(int, input().split()))
 
 cnt = 0
 curr = 0
-while curr < n - 1:
+while curr < n:
+
     if arr[curr] == 1:
         curr += 2 * m + 1
         cnt += 1
     
+    else:
+        curr += 1
 print(cnt)
