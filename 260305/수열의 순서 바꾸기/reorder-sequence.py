129f8cdfcc484c76d0a230cacf532a7e104b8a48
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-03-05 18:37:32 +0900

    Title: 수열의 순서 바꾸기, Time: 64ms, Memory: 16MB, Status: Passed - Codetree

--- 260305/수열의 순서 바꾸기/reorder-sequence.py
+++ 260305/수열의 순서 바꾸기/reorder-sequence.py
@@ -3,7 +3,7 @@ sequence = list(map(int, input().split()))
 
 idx = n - 2
 
-while idx >= 0 and sequence[idx] < sequence[idx-1]:
+while idx >= 0 and sequence[idx] < sequence[idx + 1]:
     idx -= 1
 
 print(idx + 1)
