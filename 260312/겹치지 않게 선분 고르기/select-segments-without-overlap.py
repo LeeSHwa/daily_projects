95f6981e19a46a162e7c4e7293d04c44931726c8
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-03-12 15:11:12 +0900

    Title: 겹치지 않게 선분 고르기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260312/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 260312/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -1,12 +1,11 @@
 n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
-visited = [False] * n
 array = [0] * 1001
 ans = 0
 
 def is_duplicated(a1, a2):
     for idx in range(a1, a2 + 1):
-        if array[idx] > 1:
+        if array[idx] > 0:
             return True
     
     return False
@@ -15,45 +14,21 @@ def apply_var(x1, x2, var):
     for idx in range(x1, x2 + 1):
         array[idx] += var
 
-def recurr(dep, cnt):    
+def recurr(dep, cnt):
     global ans
     if dep == n:
         ans = max(ans, cnt)
         return
-    
-    if visited[dep]:
-        recurr(dep + 1, cnt)
-        return
-    
+        
     curr_x1, curr_x2 = lines[dep]
 
-    # 1. 적용
-    visited[dep] = True
-    apply_var(curr_x1, curr_x2, 1)
-
-    # 2. 재귀
-    if is_duplicated(curr_x1, curr_x2):
-        apply_var(curr_x1, curr_x2, -1)
-        recurr(dep + 1, cnt)
-    else:
+    if not is_duplicated(curr_x1, curr_x2):
+        apply_var(curr_x1, curr_x2, 1)
         recurr(dep + 1, cnt + 1)
+        apply_var(curr_x1, curr_x2, -1)
+    
+    recurr(dep + 1, cnt)
 
-    # 3. 반환
-    visited[dep] = False
-    apply_var(curr_x1, curr_x2, -1)
+recurr(0, 0)
 
-for i in range(n):
-    base_x1, base_x2 = lines[i]
-
-    # 1. 적용
-    apply_var(base_x1, base_x2, 1)
-    visited[i] = True
-
-    # 2. 재귀
-    recurr(0, 0)
-
-    # 3. 반환
-    visited[i] = False
-    apply_var(base_x1, base_x2, 2)
-
-print(ans + 1)
+print(ans)
