1b769613c16c559df0e76b0f40304142d70e5f59
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-03-16 19:24:36 +0900

    Title: 사다리 타기, Time: 86ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260316/사다리 타기/ladder-game.py
+++ 260316/사다리 타기/ladder-game.py
@@ -1,66 +1,76 @@
-from collections import defaultdict
-
 n, m = map(int, input().split())
-height_dict = defaultdict(list)
 
 ladders = [tuple(map(int, input().split())) for _ in range(m)]
 
-grid = [[0 for _ in range(n)] for _ in range(16)]
+MAX_HEIGHT = 16
 
-def apply_grid(row, col, val): # val 1 : apply / va2 -1 : return
-    
+grid = [[0] * n for _ in range(MAX_HEIGHT)]
+
+
+def apply_grid(row, col, val):
     if val == 1:
-        grid[row][col] = 1
-
+        grid[row - 1][col - 1] = 1
     else:
-        grid[row][col] = 0
-        
+        grid[row - 1][col - 1] = 0
+
 def play_game():
     pos = list(range(1, n + 1))
 
-    for depth in range(16):
+    for depth in range(MAX_HEIGHT):
         line = grid[depth]
-        
+
         for idx in range(n - 1):
             if line[idx]:
                 pos[idx], pos[idx + 1] = pos[idx + 1], pos[idx]
     
     return pos
 
+# 1. 정답 배열을 구하기 위해 apply 호출
 for i in range(m):
-    c, r = ladders[i]
+    col, row = ladders[i]
+    apply_grid(row, col, 1)
 
-    apply_grid(r - 1, c - 1, 1)
+# 2. 정답배열 저장
+ans_pos = play_game()
 
-answer_pos = play_game()
-min_cnt = m
+# 3. 원상복구
+for i in range(m):
+    col, row = ladders[i]
+    apply_grid(row, col, -1)
 
-def recurr(depth, cnt):
+min_cnt = n
+
+# 재귀
+def recurr(elem_idx, curr_cnt):
     global min_cnt
-
-    # 0. 종료조건
-    if depth == m:
+    
+    # 0. 가지치기
+    if curr_cnt >= min_cnt:
+        return
+    
+    # 1. 종료조건
+    if elem_idx == n:
         curr_pos = play_game()
-
-        if curr_pos == answer_pos:
-            min_cnt = min(min_cnt, cnt)
-
+        
+        if curr_pos == ans_pos:
+            min_cnt = curr_cnt
+        
         return
 
-    c, r = ladders[depth]
-    
-    # 1. 제거
-    apply_grid(r - 1, c - 1, -1)
+    c, r = ladders[elem_idx]
 
-    # 2. 제거한 상태에서 탐색
-    recurr(depth + 1, cnt - 1)
+    # 2. 적용
+    apply_grid(r, c, 1)
 
-    # 3. 복구
-    apply_grid(r - 1, c - 1, 1)
+    # 3. 적용 후탐색
+    recurr(elem_idx + 1, curr_cnt + 1)
 
-    # 4. 제거하지 않은 상태에서 탐색
-    recurr(depth + 1, cnt)
+    # 4. 해제
+    apply_grid(r, c, -1)
 
-recurr(0, min_cnt)
+    # 5. 적용하지 않고 탐색
+    recurr(elem_idx + 1, curr_cnt)
+
+recurr(0, 0)
 
 print(min_cnt)
