4d400516901c58a50c5db4709705876a06a34d7c
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-03-24 14:19:00 +0900

    Title: 단순한 동전 챙기기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260324/단순한 동전 챙기기/collect-coins-easy.py
+++ 260324/단순한 동전 챙기기/collect-coins-easy.py
@@ -19,6 +19,9 @@ exist.sort()
 visited = []
 ans = float('inf')
 
+def cal_dist(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
 def backtrack(pos, idx, move_count):
     global ans
     
@@ -29,19 +32,19 @@ def backtrack(pos, idx, move_count):
         return
     # 종료조건
     if len(visited) == 3:
-        distance = move_count + abs(row - position['end'][0]) + abs(col - position['end'][1])
-        if ans > distance:
-            ans = distance
+        ans = min(ans, move_count + cal_dist(position['end'], pos))
         return
     
     # 탐색이 모두 끝났을 때
     if idx == len(exist):
         return
 
+    # 1. 골랐을 때
     visited.append(exist[idx])
-    backtrack(position[exist[idx]], idx + 1, move_count + abs(row - position[exist[idx]][0]) + abs(col - position[exist[idx]][1]))
+    backtrack(position[exist[idx]], idx + 1, move_count + cal_dist(position[exist[idx]], pos))
     visited.pop()
 
+    # 2. 고르지 않았을 때
     backtrack(pos, idx + 1, move_count)
 
 backtrack(position['start'], 0, 0)
