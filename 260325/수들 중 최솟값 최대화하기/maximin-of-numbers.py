95de0ad45f0c26b8337d8af65d2720e2a9c365e6
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-03-25 14:11:26 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 319ms, Memory: 24MB, Status: Passed - Codetree

---
+++ 260325/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [False] * n
+
+max_value = 0
+
+def backtrack(row, curr_min_value):
+    global max_value
+    
+    if row == n:
+        max_value = max(max_value, curr_min_value)
+
+    
+    for col in range(n):
+        if visited[col]:
+            continue
+        
+        visited[col] = True
+        
+        backtrack(row + 1, min(curr_min_value, grid[row][col]))
+        
+        visited[col] = False
+
+backtrack(0, float('inf'))
+
+print(max_value)
