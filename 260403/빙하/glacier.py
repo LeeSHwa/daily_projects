6f65e1800075a14737d3dafaac3ee50bb1b3854c
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-03 16:44:28 +0900

    Title: 빙하, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260403/빙하/glacier.py
+++ 260403/빙하/glacier.py
@@ -1,49 +1,54 @@
-# 그냥 무조건 0, 0에서 시작해서 닿는 1들의 위치만 따로 기록함 to_melt
-# 그 다음에 to_melt만 녹이는거지, cnt 올리고
-# 한 번 쫙 순회하면서 1 몇 개 있는 지 기록도 하고.
+# 1. 시작시 테두리는 항상 0임 
+# 그렇기 때문에 겉만 핥으면서 이게 air인지, glacier인지 기록하는 두 개의 queue가 필요
+# air를 기준으로 bfs를 돌고
+# 탐색을 끝마쳤다면 겉부분에 있는 glacier 큐만 남았을거임
+# 탐색 밖에서 glacier의 길이를 재고, last로 저장해 둔 다음, 실제 grid의 값을 변경
+# 그리고 air = glacier를 통해, 시작점을 정해줌
 
 from collections import deque
 
+gla_q = deque()
+
 n, m = map(int, input().split())
+
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-def bfs(shell):
-    to_melt = []
-    dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    q = deque()
-    q.append((cnt, cnt))
-    visited = [[False] * m for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
-    while q:
-        row, col = q.popleft()
-        
-        for dr, dc in dirs:
+def bfs(air_q):
+    gla_q = deque()
+
+    while air_q:
+        row, col = air_q.popleft()
+
+        for dr, dc in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
             nr = row + dr
             nc = col + dc
-
-            if shell <= nr < n - shell and shell <= nc < m - shell and not visited[nr][nc]:
-                if grid[nr][nc] == 1:
-                    to_melt.append((nr, nc))
-                
+            
+            if 0 <= nr < n and 0 <= nc < m and not visited[nr][nc]:
+                if grid[nr][nc]:
+                    gla_q.append((nr, nc))
                 else:
-                    q.append((nr, nc))
+                    air_q.append((nr, nc))
                 
                 visited[nr][nc] = True
-        
-    return to_melt
-
-last = -1
-cnt = 0
-
-while True:
-    array = bfs(cnt)
     
-    if len(array) == 0:
+    return gla_q
+
+last = -1 
+cnt = 0
+air_q = deque()
+air_q.append((0, 0))
+
+while last != 0:
+
+    gla_q = bfs(air_q)
+    
+    if len(gla_q) == 0:
         print(cnt, last)
         break
     
-    for r, c in array:
-        grid[r][c] = 0
-    
-    last = len(array)
     cnt += 1
+    last = len(gla_q)
+
+    air_q = gla_q
