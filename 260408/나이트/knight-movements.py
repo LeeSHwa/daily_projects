4d585a7efb0c6a0ff57fd75f3fd73162d565ca47
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-08 11:15:51 +0900

    Title: 나이트, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260408/나이트/knight-movements.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+r1, c1, r2, c2 = r1 - 1, c1 - 1, r2 - 1, c2 - 1
+dirs = [(-2, -1), (-1, -2), (-2, 1), (-1, 2), (1, 2), (2, 1), (1, -2), (2, -1)]
+
+grid = [[0] * n for _ in range(n)]
+
+visited = [[False] * n for _ in range(n)]
+
+q = deque()
+q.append((r1, c1))
+
+def bfs():
+    
+    while q:
+        row, col = q.popleft()
+        
+        visited[row][col] = True
+
+        for dr, dc in dirs:
+            nr = row + dr
+            nc = col + dc
+
+            if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc]:
+                grid[nr][nc] = grid[row][col] + 1
+                visited[nr][nc] = True
+                q.append((nr, nc))
+            
+            if grid[r2][c2]:
+                return grid[r2][c2]
+    
+    return -1
+
+
+print(bfs())
